cce3b51b6474ea04fd08bb27d85059ee0a87f9e8
S Sanjeeva <mailtosanjeeva15@gmail.com>
2026-08-27 13:57:39 +0530

    Add files via upload

---
+++ Day1 Task/script.js
@@ -0,0 +1,18 @@
+let studentName = "Sanjeeva";
+let age = 21;
+let mark = 85;
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Mark:", mark);
+
+let name = prompt("Enter your name:");
+alert("Hello " + name);
+
+let userAge = prompt("Enter your age:");
+console.log("Your age is:", userAge);
+
+let answer = confirm("Do you like JavaScript?");
+console.log("Answer:", answer);
+
+let userName = prompt("Enter your name:");
+document.writeln("Welcome, " + userName);
